9c3cea2c4ced6d7a44f225fc00e673391cab2d8b
afif ahmed <afifahmed456123@gmail.com>
2021-05-11 07:25:12 +0530

    basic file structure setup

---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,15 @@
+// This is the component we can inport in App.tsx so that we dont have to
+// set up routing in App.tsx
+
+import React from 'react';
+import Landing from '../pages/Landing';
+
+const MainComponent = (): JSX.Element => {
+	return (
+		<div>
+			<Landing />
+		</div>
+	);
+};
+
+export default MainComponent;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing  = () : JSX.Element => {
+ return(
+   <div>
+     <h1>Landing page</h1>
+   </div>
+ ) 
+}
+
+export default Landing;

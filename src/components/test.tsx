c63f2c8d2d99614e0b7ba5496a886bb95dce4329
afif ahmed <afifahmed456123@gmail.com>
2021-05-12 14:27:28 +0530

    added routing rules for development and production

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,13 +1,46 @@
-import * as React from 'react'
+import * as React from 'react';
 
-const TestButtons = () : JSX.Element => {
-  return(
-    <div>
-        <a href="http://localhost:8888/.netlify/functions/auth/github" className="w3-button w3-black">GITHUB</a>
-        &ensp;
-        <a href="http://localhost:8888/.netlify/functions/auth/google" className="w3-button w3-black">GOOGLE</a>
-    </div>
-  )
-}
+const TestButtons = (): JSX.Element => {
+	// console.log(process.env);
 
-export default TestButtons
+	return (
+		<div>
+			{process.env.NODE_ENV !== 'development' ? (
+				<div>
+					<a
+						href='https://inovact-authentication-serverless/api/auth/github'
+						className='w3-button w3-black'
+					>
+						GITHUB
+					</a>
+					&ensp;
+					<a
+						href='https://inovact-authentication-serverless/api/auth/google'
+						className='w3-button w3-black'
+					>
+						GOOGLE
+					</a>
+				</div>
+			) : (
+				// eslint-disable-next-line jsx-a11y/anchor-has-content
+				<div>
+					<a
+						href='http://localhost:8888/.netlify/functions/auth/github'
+						className='w3-button w3-black'
+					>
+						GITHUB
+					</a>
+					&ensp;
+					<a
+						href='http://localhost:8888/.netlify/functions/auth/google'
+						className='w3-button w3-black'
+					>
+						GITHUB
+					</a>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default TestButtons;

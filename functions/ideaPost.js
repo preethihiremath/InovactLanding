a38c875357f6b37ff3b872b4c2945a09c22f1d98
Shivu D G <dgshivu3232@gmail.com>
2021-05-18 18:23:54 +0530

    posting an idea now works

--- functions/ideaPost.js
+++ functions/ideaPost.js
@@ -1,41 +1,69 @@
-const query = require('./utils/hasura');
+const { query } = require('./utils/hasura');
 const middy = require('middy');
 const requireAuth = require('./middlewares/auth/requireAuth');
+const { parse } = require('querystring');
 
-const ideaPost = (event) => {
+const ideaPost = async (event) => {
+    if (event.httpMethod !== 'POST') return {
+        statusCode: 405,
+        body: 'You are not using a http POST method for this endpoint.',
+        headers: {
+            Allow: 'POST',
+        },
+    }
+
+    try {
+        event.body = JSON.parse(event.body);
+    } catch (error) {
+        event.body = parse(event.body);
+    }
+
     const url = typeof (event.body.url) == 'string' ? event.body.url.trim() : false;
-    const caption = typeof (event.body.caption) == 'string' && event.body.caption.trim().length > 0 ? event.body.caption.trim() : false;
-    const description = typeof (event.body.description) == 'string' && event.body.description.trim().length > 0 ? event.body.description.trim() : "";
-    const title = typeof (event.body.title) == 'string' && event.body.title.trim().length > 0 ? event.body.title.trim() : "";
+    const caption = typeof (event.body.caption) == 'string' ? event.body.caption.trim() : false;
+    const description = typeof (event.body.description) == 'string' ? event.body.description.trim() : false;
+    const title = typeof (event.body.title) == 'string' ? event.body.title.trim() : false;
     const user = typeof (event.user) == 'object' ? event.user : false;
 
     if (user && title) {
-        return new Promise(async (resolve, reject) => {
-            const { idea } = await query({
-                query: `
-                    mutation add_idea {
-                        insert_idea(objects: [
-                            {
-                                url: "${url}"
-                                title: "${title}"
-                                description: "${description}"
-                                caption: "${caption}"
-                                user_id: "${user.id}"
-                            }
-                        ]) {
-                            returning {
-                                id
-                            }
+        const result = await query({
+            query: `
+                mutation add_idea {
+                    insert_idea(objects: [
+                        {
+                            url: "${url}"
+                            title: "${title}"
+                            description: "${description}"
+                            caption: "${caption}"
+                            user_id: 3
+                        }
+                    ]) {
+                        returning {
+                            id
+                            title
+                            caption
+                            description
+                            url
                         }
                     }
-                `
-            });
-
-            resolve({
-                statusCode: 201,
-                body: JSON.stringify(idea)
-            });
+                }
+            `
         });
+
+        if (!result.errors) {
+            const postedValue = result.data.insert_idea.returning[0];
+
+            return({
+                statusCode: 201,
+                body: JSON.stringify(postedValue)
+            });
+        } else {
+            return {
+                statusCode: 422,
+                body: JSON.stringify({
+                    errors: result.errors.map((error) => error.message)
+                })
+            }
+        }
     } else {
         return {
             statusCode: 400,
@@ -44,4 +72,4 @@ const ideaPost = (event) => {
     }
 }
 
-exports.handler = middy(ideaPost).use(requireAuth);
+exports.handler = middy(ideaPost).use(requireAuth());
